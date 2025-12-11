58436eb98d6d02b256d021958661842843df94ec
ahmadtal20200 <ahmad.tal.dev@gmail.com>
2025-12-11 16:50:27 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="font-mono">Ahmad Altal Frontend Developer and UI/UX Designer</h1>
+    </div>
+  );
+}
+
+export default App;
